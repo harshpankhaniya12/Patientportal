7d3616c7da0c96dd2370829cb5b11f54f01fdef4
harshpankhaniya12 <136746062+harshpankhaniya12@users.noreply.github.com>
2025-03-01 14:45:31 +0530

    .

--- Pages/Calendar/Index.cshtml.cs
+++ Pages/Calendar/Index.cshtml.cs
@@ -23,7 +23,6 @@ namespace Patientportal.Pages.Calendar
             _apiService = apiService;
         }
         public async Task OnGet()
-        
         {
             string apiUrl2 = "http://ec2-13-200-161-197.ap-south-1.compute.amazonaws.com:8888/api/v1/Appointment/GetAppointmentsByDoctor";
             string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwianRpIjoiYjk5MDM2ZWItNTRhZS00ZWE0LWI1MjMtNThmYThlM2UzMzdkIiwibmJmIjoxNzQwNTU2NDQ1LCJleHAiOjE3NzIwOTI0NDUsImlhdCI6MTc0MDU1NjQ0NSwiaXNzIjoiQ29ubmV0d2VsbENJUyIsImF1ZCI6IkNvbm5ldHdlbGxDSVMifQ.yex9R3CP67Mkp715Y61FEIUIFhtiQhGJa8X01V_vEd_c9PuKw4uZbEi3_bQtpzQpwukb5uS_SPi4TN2HGh_JBQ"; // Valid token yahan dalein
